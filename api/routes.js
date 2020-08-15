29519eea8a89d103f22436f03501a77015e261e0
marcoruggeri <4high20@gmail.com>
2020-08-15 06:24:37 +0100

    Fix removeCourse and signin/signup validation

--- api/routes.js
+++ api/routes.js
@@ -1,253 +1,336 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const { check, validationResult } = require('express-validator/check');
-const bcryptjs = require('bcryptjs');
-const auth = require('basic-auth');
-const { Sequelize, sequelize, models } = require('./db');
+const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+const bcryptjs = require("bcryptjs");
+const auth = require("basic-auth");
+const { Sequelize, sequelize, models } = require("./db");
 const { User, Course } = models;
 
 const router = express.Router();
 
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
 /**
  * This authenticates the user based on the credentials parsed from the request object
- * @param {Object} req 
- * @param {Object} res 
- * @param {Object} next 
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Object} next
  */
 const authenticate = async (req, res, next) => {
-    try {
-        let message = null;
-        let user;
-        const credentials = auth(req);
-        if (credentials) {
-            user = await User.findOne({ where: { emailAddress: credentials.name }});
-            if (user) {
-                const authenticated = await bcryptjs.compare(credentials.pass, user.password);
-                if (authenticated) {
-                    console.log(`Authentication successful for username: ${user.firstName + " " + user.lastName}`);
-                    req.currentUser = user;
-                } else {
-                    message = `Authentication failure for user: ${user.emailAddress}`;
-                }
-            } else {
-                message = `User not found for username: ${user.emailAddress}`;
-            }
-        } else {
-            message = 'Auth header not found';
-        }
-        if (message) {
-            console.warn(message);
-            res.status(401).json({ message: 'Access Denied' });
-        } else {
-            next()
-        }
-    } catch (error) {
-        next(error)
+  let message = null;
+
+  const credentials = auth(req);
+  if (credentials) {
+    const users = await models.User.findAll();
+    const user = users.find((u) => u.emailAddress === credentials.name);
+    if (user) {
+      const authenticated = bcryptjs.compareSync(
+        credentials.pass,
+        user.password
+      );
+      if (authenticated) {
+        console.log(
+          `Authentication successful for emailAdress: ${user.emailAddress}`
+        );
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for Postman username: ${user.emailAddress}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
     }
-}
+  } else {
+    message = "Auth header not found";
+  }
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: "Access Denied" });
+  } else {
+    next();
+  }
+};
 
 /**
  * Makes sure that the minimum parameters are supplied in the request body
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  */
 const validate = (req, res, next) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            const errorMessages = errors.array().map(error => error.msg);
-            res.status(400).json({ errors: errorMessages });
-        } else{
-            next()
-        }
-    } catch (err) {
-        next(err)
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      next();
     }
-}
-
+  } catch (err) {
+    next(err);
+  }
+};
 
 // Returns the currently authenticated user
-router.get('/users', authenticate, async (req, res, next) => {
-    try {
-        const credentials = auth(req);
-        const user = await User.findOne({where: { emailAddress: credentials.name }, attributes: ['id','firstName','lastName','emailAddress']});
-        res.json(user); 
-    } catch (err) {
-        next(err)
-    }
-});
+router.get(
+  "/users",
+  authenticate,
+  asyncHandler(async (req, res) => {
+    const user = req.currentUser;
+    res.json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+    });
+  })
+);
 
 // Route that creates a new user.
-router.post('/users', [
-    check('firstName')
-      .exists()
+router.post(
+  "/users",
+  [
+    check("firstName")
+      .exists({ checkNull: true, checkFalsy: true })
       .withMessage('Please provide a value for "firstName"'),
-    check('lastName')
-        .exists()
-        .withMessage('Please provide a value for "lastName"'),
-    check('emailAddress')
-      .exists()
-      .withMessage('Please provide a value for "emailAddress"'),
-    check('password')
-      .exists()
+    check("lastName")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "lastName"'),
+    check("emailAddress")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "emailAddress"')
+      .isEmail()
+      .withMessage('Email must be a valid "email address" '),
+    check("password")
+      .exists({ checkNull: true, checkFalsy: true })
       .withMessage('Please provide a value for "password"'),
-  ], validate, (req, res, next) => {
-      try {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.emailAddress)) {
-            next();
-        } else {
-            res.status(400).json({error: "This email address is not valid"});
-        } 
-      } catch (err) {
-         next(err) 
-      }
+  ],
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  }, async (req, res, next) => {
-      try {
-        const existingUser = await User.findOne({where: {emailAddress: req.body.emailAddress}});
-        if (existingUser) {
-            res.status(400).json({error: "This email address is already in use"});
-        } else {
-            next();
-        }
-      } catch (err) {
-          next(err);
-      }
-  }, async (req, res) => {
-    try {
-        const hashed = await  bcryptjs.hash(req.body.password, 10); 
-        const newUser = await User.create({firstName: req.body.firstName, lastName: req.body.lastName, emailAddress: req.body.emailAddress, password: hashed});
-        
-        // Set the location to '/', the status to 201 Created, and end the response.
-        return res.status(201).location(`/`).end();
-    } catch (error) {
-        console.log(error);
-        res.json({error: `${error}`})
+    if (!errors.isEmpty()) {
+      // Use the Array `map()` method to get a list of error messages.
+      const errorMessages = errors.array().map((error) => error.msg);
+      // Returns validation errors to the client.
+      return res.status(400).json({ errors: errorMessages });
     }
-    
-});
 
+    // Hash the new
+    const password = bcryptjs.hashSync(req.body.password);
+
+    const user = await models.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailAddress: req.body.emailAddress,
+      password,
+    });
+
+    console.log(user);
+
+    // Set the status to 201 Created and end the response.
+    res.location("/").status(201).end();
+  })
+);
 /**
  * returns all courses in json format after authenticating the user
  */
-router.get('/courses', async (req, res, next) => {
-    try {
-        const courses = await Course.findAll({attributes: ['id','title','description','estimatedTime','materialsNeeded'], include: [{ model: User, attributes: ['id', 'firstName', 'lastName', 'emailAddress'] }]});
-        res.json(courses);  
-    } catch (err) {
-        next(err);
-    }
-
+router.get("/courses", async (req, res, next) => {
+  try {
+    const courses = await Course.findAll({
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "firstName", "lastName", "emailAddress"],
+        },
+      ],
+    });
+    res.json(courses);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
  * returns a specific course after authentication is successful
  */
-router.get('/courses/:id', async (req, res, next) => {
-    try {
-        let message;
-        const course = await Course.findOne({where: { id: req.params.id }, attributes: ['id','title','description','estimatedTime','materialsNeeded'], include: [{ model: User, attributes: ['id', 'firstName', 'lastName', 'emailAddress'] }]});
-        if (course != null) {
-            message = res.status(200).json(course);
-        } else {
-            message = res.status(404).json({error: "We were unable to find the course you requested"});
-        }
-        return message
-    } catch (err) {
-        next(err);
+router.get("/courses/:id", async (req, res, next) => {
+  try {
+    let message;
+    const course = await Course.findOne({
+      where: { id: req.params.id },
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "firstName", "lastName", "emailAddress"],
+        },
+      ],
+    });
+    if (course != null) {
+      message = res.status(200).json(course);
+    } else {
+      message = res
+        .status(404)
+        .json({ error: "We were unable to find the course you requested" });
     }
-
+    return message;
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
  * creates a new course and assigns it to either the current user or a user specified in the body
  */
-router.post('/courses',  [
-    check('title')
+router.post(
+  "/courses",
+  [
+    check("title").exists().withMessage('Please provide a value for "title"'),
+    check("description")
       .exists()
-      .withMessage('Please provide a value for "title"'),
-    check('description')
-        .exists()
-        .withMessage('Please provide a value for "description"'),
-  ], validate, authenticate, async (req, res, next) => {
+      .withMessage('Please provide a value for "description"'),
+  ],
+  validate,
+  authenticate,
+  async (req, res, next) => {
     try {
-        const credentials = auth(req);
-        const currentUser = await User.findOne({where: {emailAddress: credentials.name}})
-        const newCourse = await Course.create({title: req.body.title, description: req.body.description, estimatedTime: req.body.estimatedTime || null, materialsNeeded: req.body.materialsNeeded || null, userId: req.body.userId ? req.body.userId : currentUser.id});
-        
-        // Set the location to '/courses/id', the status to 201 Created, and end the response.
-        return res.status(201).location(`/courses/${newCourse.id}`).end();
-    } catch (error) {
-        console.log(error);
-        res.json({error: `${error}`});
-    }
-})
+      const credentials = auth(req);
+      const currentUser = await User.findOne({
+        where: { emailAddress: credentials.name },
+      });
+      const newCourse = await Course.create({
+        title: req.body.title,
+        description: req.body.description,
+        estimatedTime: req.body.estimatedTime || null,
+        materialsNeeded: req.body.materialsNeeded || null,
+        userId: req.body.userId ? req.body.userId : currentUser.id,
+      });
 
+      // Set the location to '/courses/id', the status to 201 Created, and end the response.
+      return res.status(201).location(`/courses/${newCourse.id}`).end();
+    } catch (error) {
+      console.log(error);
+      res.json({ error: `${error}` });
+    }
+  }
+);
 
 /**
  * updates a specific course after validating, authenticating, and comparing the user and the request
  */
-router.put('/courses/:id', [
-    check('title')
+router.put(
+  "/courses/:id",
+  [
+    check("title").exists().withMessage('Please provide a value for "title"'),
+    check("description")
       .exists()
-      .withMessage('Please provide a value for "title"'),
-    check('description')
-        .exists()
-        .withMessage('Please provide a value for "description"'),
-  ], validate, authenticate, async (req, res, next) => {
+      .withMessage('Please provide a value for "description"'),
+  ],
+  validate,
+  authenticate,
+  async (req, res, next) => {
     try {
-        const credentials = auth(req);
-        const user = await User.findOne({where: {emailAddress: credentials.name}});
-        const course = await Course.findOne({where: {id: req.params.id}});
-        if (user.id === course.userId) {
-            next();
-        } else {
-            res.status(403).json({error: "The course you are attempting to modify is owned by a different user"})
-        }    
+      const credentials = auth(req);
+      const user = await User.findOne({
+        where: { emailAddress: credentials.name },
+      });
+      const course = await Course.findOne({ where: { id: req.params.id } });
+      if (user.id === course.userId) {
+        next();
+      } else {
+        res.status(403).json({
+          error:
+            "The course you are attempting to modify is owned by a different user",
+        });
+      }
     } catch (err) {
-        next(err)
+      next(err);
     }
-
-    }, async (req, res, next) => {
-        try {
-            const updates = req.body;
-            const currentCourse = await Course.findOne({where: {id: req.params.id}});
-            const updatedCourse = await Course.update({title: updates.title, description: updates.description, estimatedTime: updates.estimatedTime ? updates.estimatedTime : currentCourse.estimatedTime, materialsNeeded: updates.materialsNeeded ? updates.materialsNeeded : currentCourse.materialsNeeded, userId: updates.userId ? updates.userId : currentCourse.userId}, {where: {id: req.params.id}});
-            return res.status(204).end();
-        } catch (err) {
-            next(err)
-        }
-
-});
+  },
+  async (req, res, next) => {
+    try {
+      const updates = req.body;
+      const currentCourse = await Course.findOne({
+        where: { id: req.params.id },
+      });
+      const updatedCourse = await Course.update(
+        {
+          title: updates.title,
+          description: updates.description,
+          estimatedTime: updates.estimatedTime
+            ? updates.estimatedTime
+            : currentCourse.estimatedTime,
+          materialsNeeded: updates.materialsNeeded
+            ? updates.materialsNeeded
+            : currentCourse.materialsNeeded,
+          userId: updates.userId ? updates.userId : currentCourse.userId,
+        },
+        { where: { id: req.params.id } }
+      );
+      return res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 /**
  * deletes a course after authenticating and comparing the user and the request
  */
-router.delete('/courses/:id', authenticate, async (req, res, next) => {
+router.delete(
+  "/courses/:id",
+  authenticate,
+  async (req, res, next) => {
     try {
-        const credentials = auth(req);
-        const user = await User.findOne({where: {emailAddress: credentials.name}});
-        const course = await Course.findOne({where: {id: req.params.id}});
-        if (user.id === course.userId) {
-            next();
-        } else {
-            res.status(403).json({error: "The course you are attempting to modify is owned by a different user"})
-        }    
+      const credentials = auth(req);
+      const user = await User.findOne({
+        where: { emailAddress: credentials.name },
+      });
+      const course = await Course.findOne({ where: { id: req.params.id } });
+      if (user.id === course.userId) {
+        next();
+      } else {
+        res.status(403).json({
+          error:
+            "The course you are attempting to modify is owned by a different user",
+        });
+      }
     } catch (err) {
-        next(err)
+      next(err);
     }
-
-}, async (req, res, next) => {
+  },
+  async (req, res, next) => {
     try {
-        const deletedCourse = await Course.destroy({where: {id: req.params.id}});
-        res.status(204).end();  
+      const deletedCourse = await Course.destroy({
+        where: { id: req.params.id },
+      });
+      res.status(204).end();
     } catch (err) {
-        next(err)
+      next(err);
     }
-
-})
+  }
+);
 
 module.exports = router;
 
--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,102 +1,118 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+
+let removeCourse;
 
 export default class CourseDetail extends Component {
-    state = {
-        courseDetail: {},
-        authorized: false,
-    }
+  state = {
+    courseDetail: {},
+    authorized: false,
+  };
 
-    componentDidMount(){
-        this.display();
-    }
+  componentDidMount() {
+    this.display();
+    removeCourse = this.props.context.actions.removeCourse;
+  }
 
-    render() {
-        const {
-            courseDetail,
-            authorized,
-        } = this.state;
+  handleClick = async (courseDetail) => {
+    await removeCourse(this.state.courseDetail.id);
+    this.props.history.push("/");
+  };
 
-        const {
-            removeCourse
-        } = this.props.context.actions;
+  render() {
+    const { courseDetail, authorized } = this.state;
 
-        return (
-            <div>
-                <div className="actions--bar">
-                    <div className="bounds">
-                        <div className="grid-100">
-                            <span>
-                                {authorized ? 
-                                    <React.Fragment>
-                                        <Link className="button" to={`/courses/${courseDetail.id}/update`}>Update</Link>
-                                        <Link className="button" onClick={() => {
-                                            removeCourse(courseDetail.id);
-                                            }} to="/" >Delete</Link>
-                                        <Link className="button button-secondary" to="/">Return to List</Link>
-                                    </React.Fragment>
-                                :
-                                    <Link className="button button-secondary" to="/">Return to List</Link>
-                                }
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="bounds course--detail">
-                    <div className="grid-66">
-                        <div className="course--header">
-                            <div className="course--label">Course</div>
-                            <div className="course--title">{courseDetail.title}</div>
-                            <p>{courseDetail.User ? `By ${courseDetail.User.firstName} ${courseDetail.User.lastName}`: 'By anonymous'}</p>
-                        </div>
-                        <ReactMarkdown className="course--description" source={courseDetail.description} />
-                    </div>
-                    <div className="grid-25 grid-right">
-                        <div className="course--stats">
-                            <ul className="course--stats--list">
-                                <li className="course--stats--list--item">
-                                    <h4>Estimated Time</h4>
-                                    <h3>{courseDetail.estimatedTime}</h3>
-                                </li>
-                                <li className="course--stats--list--item">
-                                    <h4>Materials Needed</h4>
-                                    <ReactMarkdown source={courseDetail.materialsNeeded} />
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div>
+        <div className="actions--bar">
+          <div className="bounds">
+            <div className="grid-100">
+              <span>
+                {authorized ? (
+                  <React.Fragment>
+                    <Link
+                      className="button"
+                      to={`/courses/${courseDetail.id}/update`}
+                    >
+                      Update
+                    </Link>
+                    <button className="button" onClick={this.handleClick}>
+                      Delete
+                    </button>
+                    <Link className="button button-secondary" to="/">
+                      Return to List
+                    </Link>
+                  </React.Fragment>
+                ) : (
+                  <Link className="button button-secondary" to="/">
+                    Return to List
+                  </Link>
+                )}
+              </span>
             </div>
-        )
-    }
+          </div>
+        </div>
+        <div className="bounds course--detail">
+          <div className="grid-66">
+            <div className="course--header">
+              <div className="course--label">Course</div>
+              <div className="course--title">{courseDetail.title}</div>
+              <p>
+                {courseDetail.User
+                  ? `By ${courseDetail.User.firstName} ${courseDetail.User.lastName}`
+                  : "By anonymous"}
+              </p>
+            </div>
+            <ReactMarkdown
+              className="course--description"
+              source={courseDetail.description}
+            />
+          </div>
+          <div className="grid-25 grid-right">
+            <div className="course--stats">
+              <ul className="course--stats--list">
+                <li className="course--stats--list--item">
+                  <h4>Estimated Time</h4>
+                  <h3>{courseDetail.estimatedTime}</h3>
+                </li>
+                <li className="course--stats--list--item">
+                  <h4>Materials Needed</h4>
+                  <ReactMarkdown source={courseDetail.materialsNeeded} />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    display = () => {
-        const { context, match } = this.props;
-        context.data.getCourse(match.params.id)
-            .then(course => {
-                if (context.authenticatedUser) {
-                    if (context.authenticatedUser.id === course.User.id) {
-                        this.setState(() => {
-                            return {
-                                courseDetail: { ...course },
-                                authorized: true,
-                            }
-                        });
-                    } else {
-                        this.setState(() => {
-                            return {
-                                courseDetail: { ...course },
-                            }
-                        });
-                    }
-                } else {
-                    this.setState(() => {
-                        return {
-                            courseDetail: { ...course },
-                        }
-                    });
-                }
-            });
-    }
+  display = () => {
+    const { context, match } = this.props;
+    context.data.getCourse(match.params.id).then((course) => {
+      if (context.authenticatedUser) {
+        if (context.authenticatedUser.id === course.User.id) {
+          this.setState(() => {
+            return {
+              courseDetail: { ...course },
+              authorized: true,
+            };
+          });
+        } else {
+          this.setState(() => {
+            return {
+              courseDetail: { ...course },
+            };
+          });
+        }
+      } else {
+        this.setState(() => {
+          return {
+            courseDetail: { ...course },
+          };
+        });
+      }
+    });
+  };
 }
